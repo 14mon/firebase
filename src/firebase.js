0abcaef9288df27c4f81ad4136bf4d2b2595922c
Mon Mon <116795309+14mon@users.noreply.github.com>
2025-03-17 14:57:03 +0630

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration, replace it with your project keys
 const firebaseConfig = {
-  apiKey: "AIzaSyCHsPY_9e1o4O2Uu3AXVEUTKXD_wdEHYtQ",
-  authDomain: "biodent-myanmar.firebaseapp.com",
-  projectId: "biodent-myanmar",
-  storageBucket: "biodent-myanmar.appspot.com",
-  messagingSenderId: "29662306827	", 
-  appId: "1:29662306827:android:e3f75920b9b36dd05a5d0e" 
+  apiKey: "*********",
+  authDomain: "********",
+  projectId: "********",
+  storageBucket: "******",
+  messagingSenderId: "******", 
+  appId: "*************" 
 };
 
 
